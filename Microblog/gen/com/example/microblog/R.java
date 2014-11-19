b3b8c5eb6af162ff6881603cba925cd0518e54ac
zajzaj <1647880590@qq.com>
2014-11-19 17:34:43 +0800

    add showComment

--- Microblog/gen/com/example/microblog/R.java
+++ Microblog/gen/com/example/microblog/R.java
@@ -20,6 +20,12 @@ public final class R {
         public static final int tweet_white=0x7f040005;
     }
     public static final class dimen {
+        /** 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
         public static final int activity_horizontal_margin=0x7f050010;
         public static final int activity_vertical_margin=0x7f050011;
         public static final int border=0x7f05000f;
@@ -71,80 +77,87 @@ public final class R {
     public static final class id {
         public static final int back_btn=0x7f06000e;
         public static final int cancel_publish=0x7f060008;
-        public static final int comment_cancel_button=0x7f060032;
-        public static final int comment_comment=0x7f060031;
-        public static final int comment_confirm_publish=0x7f060030;
+        public static final int comment_cancel_button=0x7f060037;
+        public static final int comment_comment=0x7f060036;
+        public static final int comment_confirm_publish=0x7f060035;
         public static final int confirm_publish=0x7f060007;
         public static final int confirm_register=0x7f060006;
-        public static final int editText1=0x7f06003c;
-        public static final int editText2=0x7f06003d;
+        public static final int editText1=0x7f060041;
+        public static final int editText2=0x7f060042;
         public static final int enter_img=0x7f060011;
-        public static final int favor_btn=0x7f060015;
+        public static final int favor_btn=0x7f060017;
         public static final int fun_btn=0x7f06000f;
         public static final int head_layout=0x7f06000d;
-        public static final int info_layout=0x7f060017;
+        public static final int info_layout=0x7f06001b;
         public static final int item_img=0x7f060013;
         public static final int listView=0x7f060012;
         public static final int login_head=0x7f060000;
         public static final int login_password=0x7f060003;
         public static final int login_username=0x7f060002;
         public static final int loginbutton=0x7f060004;
-        public static final int message_time=0x7f060016;
-        public static final int modify_cancel_button=0x7f06003a;
-        public static final int modify_comment=0x7f060039;
-        public static final int modify_confirm_publish=0x7f060038;
-        public static final int modify_pwd_confirm_btn=0x7f060037;
-        public static final int modify_pwd_confirm_new=0x7f060036;
-        public static final int modify_pwd_new=0x7f060035;
-        public static final int modify_pwd_old=0x7f060034;
-        public static final int modify_pwd_username=0x7f060033;
-        public static final int name_text=0x7f060014;
-        public static final int new_cancel_button=0x7f06002c;
-        public static final int new_comment=0x7f06002b;
-        public static final int new_confirm_publish=0x7f06002a;
-        public static final int personal_fans_count=0x7f060027;
-        public static final int personal_focus_count=0x7f060026;
-        public static final int personal_head=0x7f060024;
-        public static final int personal_info_layout=0x7f06001b;
-        public static final int personal_item_img=0x7f06001c;
-        public static final int personal_message_time=0x7f06001e;
-        public static final int personal_name_text=0x7f06001d;
-        public static final int personal_tweet_comment=0x7f060021;
-        public static final int personal_tweet_comment_count=0x7f060023;
-        public static final int personal_tweet_message=0x7f06001f;
-        public static final int personal_tweet_repost=0x7f060020;
-        public static final int personal_tweet_repost_count=0x7f060022;
-        public static final int personal_username=0x7f060025;
+        public static final int message_time=0x7f060018;
+        public static final int modify_cancel_button=0x7f06003f;
+        public static final int modify_comment=0x7f06003e;
+        public static final int modify_confirm_publish=0x7f06003d;
+        public static final int modify_pwd_confirm_btn=0x7f06003c;
+        public static final int modify_pwd_confirm_new=0x7f06003b;
+        public static final int modify_pwd_new=0x7f06003a;
+        public static final int modify_pwd_old=0x7f060039;
+        public static final int modify_pwd_username=0x7f060038;
+        public static final int name_text=0x7f060015;
+        public static final int new_cancel_button=0x7f060031;
+        public static final int new_comment=0x7f060030;
+        public static final int new_confirm_publish=0x7f06002f;
+        public static final int personal_fans_count=0x7f06002c;
+        public static final int personal_focus_count=0x7f06002b;
+        public static final int personal_head=0x7f060029;
+        public static final int personal_info_layout=0x7f060020;
+        public static final int personal_item_img=0x7f060021;
+        public static final int personal_message_time=0x7f060023;
+        public static final int personal_name_text=0x7f060022;
+        public static final int personal_tweet_comment=0x7f060026;
+        public static final int personal_tweet_comment_count=0x7f060028;
+        public static final int personal_tweet_message=0x7f060024;
+        public static final int personal_tweet_repost=0x7f060025;
+        public static final int personal_tweet_repost_count=0x7f060027;
+        public static final int personal_username=0x7f06002a;
         public static final int register=0x7f060009;
         public static final int register_head=0x7f060001;
         public static final int register_password=0x7f06000b;
         public static final int register_repassword=0x7f06000c;
         public static final int register_username=0x7f06000a;
         public static final int repassword=0x7f060005;
-        public static final int repost_cancel_button=0x7f06002f;
-        public static final int repost_comment=0x7f06002e;
-        public static final int repost_confirm_publish=0x7f06002d;
-        public static final int textView4=0x7f06003b;
+        public static final int repost_cancel_button=0x7f060034;
+        public static final int repost_comment=0x7f060033;
+        public static final int repost_confirm_publish=0x7f060032;
+        public static final int sc_comment=0x7f06001a;
+        public static final int sc_info_layout=0x7f06001c;
+        public static final int sc_item_img=0x7f060014;
+        public static final int sc_message_time=0x7f060019;
+        public static final int sc_name_text=0x7f060016;
+        public static final int textView4=0x7f060040;
         public static final int title=0x7f060010;
-        public static final int tweet_button=0x7f06001a;
-        public static final int tweet_comment=0x7f060028;
-        public static final int tweet_content=0x7f060018;
-        public static final int tweet_message=0x7f060019;
-        public static final int tweet_repost=0x7f060029;
+        public static final int tweet_button=0x7f06001f;
+        public static final int tweet_comment=0x7f06002d;
+        public static final int tweet_content=0x7f06001d;
+        public static final int tweet_message=0x7f06001e;
+        public static final int tweet_repost=0x7f06002e;
     }
     public static final class layout {
         public static final int activity_home_page=0x7f030000;
-        public static final int comment=0x7f030001;
-        public static final int head=0x7f030002;
-        public static final int home_page_list_item=0x7f030003;
-        public static final int login=0x7f030004;
-        public static final int modify_microblog=0x7f030005;
-        public static final int modify_password=0x7f030006;
-        public static final int new_microblog=0x7f030007;
-        public static final int personal_center=0x7f030008;
-        public static final int personal_center_list_item=0x7f030009;
-        public static final int register=0x7f03000a;
-        public static final int repost=0x7f03000b;
+        public static final int activity_show_comment=0x7f030001;
+        public static final int comment=0x7f030002;
+        public static final int head=0x7f030003;
+        public static final int home_page_list_item=0x7f030004;
+        public static final int login=0x7f030005;
+        public static final int modify_microblog=0x7f030006;
+        public static final int modify_password=0x7f030007;
+        public static final int new_microblog=0x7f030008;
+        public static final int personal_center=0x7f030009;
+        public static final int personal_center_list_item=0x7f03000a;
+        public static final int register=0x7f03000b;
+        public static final int repost=0x7f03000c;
+        public static final int show_comment_list_item=0x7f03000d;
     }
     public static final class string {
         public static final int action_settings=0x7f070009;
@@ -176,6 +189,7 @@ public final class R {
         public static final int title_activity_new_microblog_acticity=0x7f070002;
         public static final int title_activity_register=0x7f070015;
         public static final int title_activity_repost=0x7f07001a;
+        public static final int title_activity_show_comment=0x7f070020;
         public static final int title_new_microblog=0x7f070003;
         public static final int tweet_message=0x7f070017;
         public static final int user_hint=0x7f07000b;
